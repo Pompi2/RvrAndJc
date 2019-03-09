668c107df822fc5e258154abba9b075f9209468b
HashikD <HashikDonthineni@gmail.com>
2019-03-10 01:10:16 +0530

    Included MyNotification Manager

---
+++ app/src/main/java/com/example/rvrandjc/models/Constants.java
@@ -0,0 +1,8 @@
+package com.example.rvrandjc.models;
+
+public class Constants {
+    public static final String[] NOT_CATEGORIES = {"I","II","III","IV","MCA-I","MCA-II","MBA","All","Faculty"};
+    public static final String CHANNEL_ID = "my_channel_01";
+    public static final String CHANNEL_NAME = "Rvr and JCCE";
+    public static final String CHANNEL_DESCRIPTION = "Notification for Rvr and JCCE";
+}
