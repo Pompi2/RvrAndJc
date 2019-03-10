25b1f6b82364f86588f92ae8747dc68a90704999
HashikD <HashikDonthineni@gmail.com>
2019-03-10 11:12:20 +0530

    Added preferences fragment

--- app/src/main/java/com/hashik/rvrandjc/views/SettingsFragment.java
+++ app/src/main/java/com/hashik/rvrandjc/views/SettingsFragment.java
@@ -1,8 +1,18 @@
 package com.hashik.rvrandjc.views;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceGroupAdapter;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceViewHolder;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,16 +24,25 @@ import com.hashik.rvrandjc.R;
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     public SettingsFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+
+    @Override
+    public void setPreferenceScreen(PreferenceScreen preferenceScreen) {
+        super.setPreferenceScreen(preferenceScreen);
+        if (preferenceScreen != null) {
+            int count = preferenceScreen.getPreferenceCount();
+            for (int i = 0; i < count; i++)
+                preferenceScreen.getPreference(i).setIconSpaceReserved(false);
+        }
     }
 }
