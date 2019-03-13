e0da9d4940901a2d2c8af4963318c907bc569679
HashikD <HashikDonthineni@gmail.com>
2019-03-13 11:29:11 +0530

    Settings notification preference to the previously set preference after enabling notifications

--- app/src/main/java/com/hashik/rvrandjc/views/SettingsFragment.java
+++ app/src/main/java/com/hashik/rvrandjc/views/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.hashik.rvrandjc.views;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 import androidx.preference.ListPreference;
@@ -13,6 +15,9 @@ import com.hashik.rvrandjc.R;
 import com.hashik.rvrandjc.models.Constants;
 import com.hashik.rvrandjc.models.MyNotificationManager;
 import com.hashik.rvrandjc.models.NotificationSubscriptionManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
@@ -44,15 +49,13 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             public boolean onPreferenceChange(Preference preference, Object newValue) {
                 boolean switched = ((SwitchPreference) preference)
                         .isChecked();
-                if(switched){
+                if (switched) {
                     myManager.unSubToAllChannels();
                     list.setEnabled(false); //Disable notification channel selection
                     Toast.makeText(getContext(), "Notifications disabled", Toast.LENGTH_SHORT).show();
-                }else{
-                    myManager.subToAllChannels();
+                } else {
                     list.setEnabled(true); //Enable notification channel selection
-                    list.setValueIndex(8); //Set notification selection to all
-                    Toast.makeText(getContext(), "Setting notification preference to all", Toast.LENGTH_SHORT).show();
+                    list.setValueIndex(getListPreviousValue()); //Set notification selection to the previously selected value
                 }
                 return true;
             }
@@ -63,31 +66,58 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 int index = list.findIndexOfValue(newValue.toString());
 
                 if (index != -1) {
-                    switch (index){
-                        case 0: myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
+                    switch (index) {
+                        case 0:
+                            myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
                             break;
-                        case 1: myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
+                        case 1:
+                            myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
                             break;
-                        case 2: myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
+                        case 2:
+                            myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
                             break;
-                        case 3: myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
+                        case 3:
+                            myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
                             break;
-                        case 4: myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
+                        case 4:
+                            myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
                             break;
-                        case 5: myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
+                        case 5:
+                            myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
                             break;
-                        case 6: myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
+                        case 6:
+                            myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
                             break;
-                        case 7: myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
+                        case 7:
+                            myManager.subToChannel(Constants.NOT_CATEGORIES[index]);
                             break;
-                        case 8: myManager.subToAllChannels();
+                        case 8:
+                            myManager.subToAllChannels();
                             break;
                     }
 
-                    Toast.makeText(getContext(),"Changed subscription to "+list.getEntries()[index], Toast.LENGTH_LONG).show();
+                    Toast.makeText(getContext(), "Changed subscription to " + list.getEntries()[index], Toast.LENGTH_LONG).show();
                 }
                 return true;
             }
         });
+    }
+
+    private int getListPreviousValue() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getContext());
+        final String category = sp.getString("not_category", null);
+        if (category == null) {
+            NotificationSubscriptionManager.getInstance().subToAllChannels();
+            Toast.makeText(getContext(), "Now you'll get all notifications", Toast.LENGTH_SHORT).show();
+            return 8;
+        } else {
+            String[] list = getResources().getStringArray(R.array.not_pref); //Getting list of categories
+            ArrayList<String> aList = new ArrayList<String>(Arrays.asList(list));
+            int index = aList.indexOf(category);
+            final String const_category = Constants.NOT_CATEGORIES[index];
+            NotificationSubscriptionManager.getInstance().subToChannel(const_category);
+            Toast.makeText(getContext(), "Setting notification preference to " + category, Toast.LENGTH_SHORT).show();
+            return index;
+        }
     }
 }
