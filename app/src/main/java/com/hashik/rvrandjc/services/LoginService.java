df726b7fedcef4bfd76913ec712abd936684fc65
HashikD <HashikDonthineni@gmail.com>
2019-03-17 19:54:32 +0530

    Added UserData class to send data in retrofit

---
+++ app/src/main/java/com/hashik/rvrandjc/services/LoginService.java
@@ -0,0 +1,15 @@
+package com.hashik.rvrandjc.services;
+
+import com.hashik.rvrandjc.models.JSONDataModels.JSONData;
+import com.hashik.rvrandjc.models.JSONDataModels.UserData;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
+public interface LoginService {
+    @GET("/posts")
+    @Headers("Content-type: application/json")
+    Call<JSONData> initiateLogin(@Body UserData user);
+}
